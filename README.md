dd2bcc915834ddb45e006ce366b9868286d65f44
Nicolas Schickert <nicolas.schickert@gmail.com>
2019-01-26 13:51:36 +0100

    Issue #2: Add some information; reformat bfi.go

---
+++ README.md
@@ -0,0 +1,30 @@
+Brainfuck
+===
+
+## Interpreter bfi
+### Usage:
+```
+bfi -h
+Usage of bfi:
+  -c string
+    	Brainfuck source code inline
+  -i string
+    	Brainfuck source code file
+```
+#### Examples
+* interpret a source code file:
+	`bfi -i source.bf`
+* directly interpret a piece of code:
+	`bfi -c "+++++ +++++[>++++++<-]>+++++."`
+* use caesar "encryption" :
+	`echo "Brainfuck" | bfi -c ",[+++.,]"`
+
+### Installation
+
+```
+git clone https://github.com/nicolas93/brainfuck.git
+cd brainfuck
+go install ./bfi
+```
+
+You can also just use `go run bfi/bfi.go`.
--- bfi/bfi.go
+++ bfi/bfi.go
@@ -1,87 +1,83 @@
 package main
 
-
-
-import ("fmt"
-		"os"
-		"flag"
-		"strings"
-		"io/ioutil"
-		"bufio")
-
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 func skip_loop(code string, i int) int {
-	count := 0 
-	for j:=i+1; j<len(code); j++{
-		if(code[j] == 93){
-			if(count == 0){
+	count := 0
+	for j := i + 1; j < len(code); j++ {
+		if code[j] == 93 {
+			if count == 0 {
 				return j
-			}else{
+			} else {
 				count--
 			}
 		}
-		if( code[j] == 91){
+		if code[j] == 91 {
 			count++
 		}
 	}
-	return len(code)-1
+	return len(code) - 1
 }
 
-
-func interpret(code string){
+func interpret(code string) {
 	field := make([]int, 30000)
 	stack := make([]int, len(code))
 	s_i := 0
 	f_i := 0
 	reader := bufio.NewReader(os.Stdin)
 	var b []byte = make([]byte, 1)
-	for i:=0; i<len(code); i++{
-		switch code[i]{
-			case 43: // +
-				field[f_i]++
-			case 45: // -
-				field[f_i]--
-			case 62: // >
-				f_i = (f_i +1) % len(code)
-			case 60: // <
-				f_i = (f_i -1)
-				if f_i < 0 {
-					f_i = len(code)-f_i
-				}
-			case 44: // ,
-			    os.Stdin.Read(b)
-			    field[f_i] = int(b[0])
-			    if(field[f_i] == 10){
-			    	field[f_i] = 0
-			    }
-			    //fmt.Println(field[f_i])
-			case 46: // .
-				fmt.Printf(string(field[f_i]))
-			case 91: // [
-				if(field[f_i] == 0){
-					i = skip_loop(code, i)
-				}else{
-					stack[s_i] = i 
-					s_i++	
-				}
-			case 93: // ]
-				i = stack[s_i-1]-1
-				s_i--
+	for i := 0; i < len(code); i++ {
+		switch code[i] {
+		case 43: // +
+			field[f_i]++
+		case 45: // -
+			field[f_i]--
+		case 62: // >
+			f_i = (f_i + 1) % len(code)
+		case 60: // <
+			f_i = (f_i - 1)
+			if f_i < 0 {
+				f_i = len(code) - f_i
+			}
+		case 44: // ,
+			os.Stdin.Read(b)
+			field[f_i] = int(b[0])
+			if field[f_i] == 10 {
+				field[f_i] = 0
+			}
+			//fmt.Println(field[f_i])
+		case 46: // .
+			fmt.Printf(string(field[f_i]))
+		case 91: // [
+			if field[f_i] == 0 {
+				i = skip_loop(code, i)
+			} else {
+				stack[s_i] = i
+				s_i++
+			}
+		case 93: // ]
+			i = stack[s_i-1] - 1
+			s_i--
 		default:
 			//fmt.Println("default")
 		}
-	if 1 == 11{ // debug
-		fmt.Println(i)
-		fmt.Println(string(code[i]))
-		fmt.Println(stack)
-		fmt.Println(field[0:10])
-		reader.ReadString('\n')
-		//fmt.Println(text)
-	}
+		if 1 == 11 { // debug
+			fmt.Println(i)
+			fmt.Println(string(code[i]))
+			fmt.Println(stack)
+			fmt.Println(field[0:10])
+			reader.ReadString('\n')
+			//fmt.Println(text)
+		}
 	}
 }
-
-
 
 func main() {
 	var code_file string
@@ -90,18 +86,17 @@ func main() {
 	flag.StringVar(&code_string, "c", "", "Brainfuck source code inline")
 	flag.Parse()
 
-	if strings.Compare(code_file, "") != 0{
+	if strings.Compare(code_file, "") != 0 {
 		b, err := ioutil.ReadFile(code_file)
-    	if err != nil {
+		if err != nil {
 			fmt.Print(err)
-   		}
-   		interpret(string(b))
-	}else if strings.Compare(code_string, "") != 0{
+		}
+		interpret(string(b))
+	} else if strings.Compare(code_string, "") != 0 {
 		interpret(code_string)
 	}
 
-
-//	interpret("+++++ +++++[>++[>+++<-]<-]>>+++++.")
-//	interpret("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
-//	interpret(",[+++.,]")
+	//	interpret("+++++ +++++[>++[>+++<-]<-]>>+++++.")
+	//	interpret("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
+	//	interpret(",[+++.,]")
 }
